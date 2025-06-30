6fdecb7b0112715c41d4014647ae3e0d39b1fedf
Paxton Terry <117777648+PaxtonTerryDev@users.noreply.github.com>
2025-06-30 15:10:49 -0600

    Adds dynamic tenant data and config to web app
    Fetches tenant profile data from Supabase and integrates app config
    Updates Next.js config to transpile new workspace packages
    Modifies Supabase config to use public environment variables

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,5 @@
+import config from "@workspace/app-config";
+import { supabase } from "@workspace/supabase-config";
 import { Button } from "@workspace/ui/components/button";
 import {
   Card,
@@ -9,18 +11,24 @@ import {
   CardTitle,
 } from "@workspace/ui/components/card";
 
-export default function Page() {
+export default async function Page() {
+  let { data: tenant_profiles, error } = await supabase
+    .from("tenant_profiles")
+    .select("*")
+    .eq("id", "fa732f4c-c906-4e5c-8f8b-c874e6968f86");
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="w-2xl h-xl">
         <Card>
           <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-            <CardAction>Card Action</CardAction>
+            <CardTitle>{config.app.name}</CardTitle>
+            <CardDescription>
+              {tenant_profiles![0]?.display_name}
+            </CardDescription>
+            <CardAction>{`https://${tenant_profiles![0]?.subdomain}.our-app.com`}</CardAction>
           </CardHeader>
           <CardContent>
-            <p>Card Content</p>
+            <p>{config.app.description}</p>
           </CardContent>
           <CardFooter>
             <p>Card Footer</p>
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
+  transpilePackages: ["@workspace/ui", "@workspace/app-config"],
 }
 
 export default nextConfig
